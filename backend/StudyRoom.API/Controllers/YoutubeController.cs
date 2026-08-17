6a5985b6534833dbc7e23540d1a612748d33f432
Your Name <you@example.com>
2026-08-17 17:53:16 +0200

    Allow music search without signing in

--- backend/StudyRoom.API/Controllers/YoutubeController.cs
+++ backend/StudyRoom.API/Controllers/YoutubeController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using StudyRoom.API.Models;
@@ -7,7 +6,6 @@ namespace StudyRoom.API.Controllers;
 
 [ApiController]
 [Route("api/youtube")]
-[Authorize]
 public class YoutubeController : ControllerBase
 {
     private readonly IHttpClientFactory _http;
